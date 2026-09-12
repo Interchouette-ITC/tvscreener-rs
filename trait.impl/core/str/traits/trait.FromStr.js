752aa6512f172757f9d555efdd06a72dfac0dc76
gRoussac <gRoussac@users.noreply.github.com>
2026-09-12 13:31:02 +0000

    Deploy documentation for dev

--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["tvscreener",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"tvscreener/filter/enum.FilterOperator.html\" title=\"enum tvscreener::filter::FilterOperator\">FilterOperator</a>",0]]]]);
+    const implementors = Object.fromEntries([["tvscreener",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"tvscreener/filter/enum.FilterOperator.html\" title=\"enum tvscreener::filter::FilterOperator\">FilterOperator</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
